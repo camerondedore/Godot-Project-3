a151e3e3e461ece34438b02a5b4c5a74397d2660
camerondedore <camerondedore@gmail.com>
2023-05-18 18:22:43 -0700

    starting player hud

---
+++ C#/PlayerHud.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class PlayerHud : Node
+{
+
+    [Export]
+    Label candiedNutsCounter,
+        dockLeavesCounter,
+        sanicleCounter,
+        rangerBandageCounter;
+
+
+
+    
+}
