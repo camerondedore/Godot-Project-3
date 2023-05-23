8717bf4f98cadacf65ed1427bcde524ab28f0345
camerondedore <camerondedore@gmail.com>
2023-05-22 19:05:13 -0700

    adding more pickup prefabs

--- C#/Pickups/SaniclePickup.cs
+++ C#/Pickups/SaniclePickup.cs
@@ -4,31 +4,31 @@ using System;
 public partial class SaniclePickup : RigidBody3D, IPickup
 {
 
-    float turnSpeed = 1.57f;
+	float turnSpeed = 1.57f;
 
 
 
-    public override void _PhysicsProcess(double delta)
-    {
-        // check for frozen rigidbody
-        if(!Freeze)
-        {
-            // exit here to avoid rotating an active rigidbody
-            return;
-        }
+	public override void _PhysicsProcess(double delta)
+	{
+		// check for frozen rigidbody
+		if(!Freeze)
+		{
+			// exit here to avoid rotating an active rigidbody
+			return;
+		}
 
-        // rotate-manually placed pickups
-        Rotate(Vector3.Up, turnSpeed * ((float) delta));
-    }
+		// rotate-manually placed pickups
+		Rotate(Vector3.Up, turnSpeed * ((float) delta));
+	}
 
 
 
-    public void PickupAction(PlayerPickup.PlayerPickupData data)
-    {
-        // add sanicle to player inventory
-        PlayerInventory.inventory.AddToInventory(0, 0, 1, 0, null);
+	public void PickupAction(PlayerPickup.PlayerPickupData data)
+	{
+		// add sanicle to player inventory
+		PlayerInventory.inventory.AddToInventory(0, 0, 1, 0, null);
 
-        // delete pickup
-        QueueFree();
-    }
+		// delete pickup
+		QueueFree();
+	}
 }
