3fcf3b454a5b29c46bb3089920860ba6f03acfdd
camerondedore <camerondedore@gmail.com>
2023-09-05 17:01:30 -0700

    flipping player audio with character audio

---
+++ C#/CharacterComplex/PlayerCharacterAudio.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+namespace PlayerCharacterComplex
+{
+    public partial class PlayerCharacterAudio : AudioTools3d
+    {
+
+        [Export]
+        AudioStream rangerBandageGatherSound,
+            rangerBandageCraftSound,
+            fallDamageSound;
+
+
+
+        public void PlayRangerBandageGatherSound()
+        {
+            PlaySound(rangerBandageGatherSound, 0.1f);
+        }
+        
+        
+        
+        public void PlayRangerBandageCraftSound()
+        {
+            PlaySound(rangerBandageCraftSound, 0.1f);
+        }
+
+
+
+        public void PlayFallDamageSound()
+        {
+            PlaySound(fallDamageSound, 0.1f);
+        }
+    }
+}
---
+++ C#/PlayerAudio.cs
@@ -0,0 +1,102 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class PlayerAudio : Node
+{
+
+    [Export]
+    AudioStream[] candiedNutPickupSounds;
+    [Export]
+    AudioStream bandageHealSound,
+        dockLeafPickupSound,
+        saniclePickupSound,
+        armorPickupSound,
+        ciderPickupSound;
+
+    List<AudioTools> audioPlayers = new List<AudioTools>();
+    int audioPlayerIndex = 0;
+
+
+
+    public override void _Ready()
+    {
+        // get audiostream children
+        foreach(AudioTools child in GetChildren())
+        {
+            audioPlayers.Add(child);
+        }
+    }
+
+
+
+    public void PlaySound(AudioStream sound, float pitchRange)
+    {
+        audioPlayers[audioPlayerIndex].PlaySound(sound, pitchRange);
+
+        // use next audio player
+        audioPlayerIndex++;
+
+        if(audioPlayerIndex >= audioPlayers.Count)
+        {
+            audioPlayerIndex = 0;
+        }
+    }
+
+
+
+    public void PlayRandomSound(AudioStream[] sounds, float pitchRange)
+    {
+        audioPlayers[audioPlayerIndex].PlayRandomSound(sounds, pitchRange);
+
+        // use next audio player
+        audioPlayerIndex++;
+
+        if(audioPlayerIndex >= audioPlayers.Count)
+        {
+            audioPlayerIndex = 0;
+        }
+    }
+
+
+
+    public void PlayBandageHealSound()
+    {
+        PlaySound(bandageHealSound, 0.1f);
+    }
+    
+    
+    
+    public void PlayCandiedNutPickupSound()
+    {
+        PlayRandomSound(candiedNutPickupSounds, 0.1f);
+    }
+
+
+
+    public void PlayDockLeafPickupSound()
+    {
+        PlaySound(dockLeafPickupSound, 0.1f);
+    }
+
+    
+
+    public void PlaySaniclePickupSound()
+    {
+        PlaySound(saniclePickupSound, 0.1f);
+    }
+
+
+
+    public void PlayArmorPickupSound()
+    {
+        PlaySound(armorPickupSound, 0.1f);
+    }
+
+
+
+    public void PlayCiderPickupSound()
+    {
+        PlaySound(ciderPickupSound, 0.1f);
+    }
+}
