519d7c915608081dccd372f5c4d252c152e274b8
camerondedore <camerondedore@gmail.com>
2023-11-19 09:53:58 -0800

    adding pickup spawner to spiderweb

--- C#/PlayerBow/SpiderwebTarget.cs
+++ C#/PlayerBow/SpiderwebTarget.cs
@@ -5,12 +5,37 @@ using PlayerBow;
 public partial class SpiderwebTarget : StaticBody3D, IBowTarget
 {
 
+    // DO NOT USE PINNED OBJECTS WITH STORED ITEM
+
     [Export]
     public string arrowType = "blade";
     [Export]
     PackedScene cutFx;
     [Export]
     Node[] pinnedObjects;
+    [Export]
+    RigidbodySpawner pickupSpawner;
+    [Export]
+    PackedScene storedItem;
+
+
+
+    public override void _Ready()
+    {
+        // check if web pins anything
+        if(pinnedObjects.Length > 0)
+        {
+            return;
+        }
+
+        // get if lockbox was already activated
+        var wasActivated = WorldData.data.CheckActivatedObjects(this);
+
+        if(wasActivated)
+        {
+            QueueFree();
+        }
+    }
 
 
 
@@ -46,10 +71,22 @@ public partial class SpiderwebTarget : StaticBody3D, IBowTarget
         GetTree().CurrentScene.AddChild(newFx);
         newFx.Owner = GetTree().CurrentScene;
 
-        // activate pinned objects
-        foreach(IActivatable i in pinnedObjects)
+        // web can either pin or store, not both at once
+        if(pinnedObjects.Length > 0)
         {
-            i.Activate();
+            // activate pinned objects
+            foreach(IActivatable i in pinnedObjects)
+            {
+                i.Activate();
+            }
+        }
+        else
+        {
+            // spawn pickup
+            pickupSpawner.Spawn(storedItem);
+
+            // save to activated objects
+            WorldData.data.ActivateObject(this);
         }
 
         // destroy
