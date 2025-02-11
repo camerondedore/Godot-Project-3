39f5a54460935723f6240a1f3624622cc758359a
camerondedore <camerondedore@gmail.com>
2025-02-11 08:30:14 -0800

    working on merchant npc

--- C#/NpcMerchant/NpcMerchantStateOffer.cs
+++ C#/NpcMerchant/NpcMerchantStateOffer.cs
@@ -5,5 +5,36 @@ namespace NonPlayerCharacter;
 
 public partial class NpcMerchantStateOffer : NpcMerchantState
 {
+
     
+
+
+
+    public override void RunState(double delta)
+    {
+        // unlock cursor
+        Input.MouseMode = Input.MouseModeEnum.Visible;
+    }
+    
+    
+    
+    public override void StartState()
+    {
+        // show ui
+    }
+
+
+
+    public override void EndState()
+    {
+        // hide ui
+    }
+
+
+
+    public override State Transition()
+    {
+
+        return this;
+    }
 }
