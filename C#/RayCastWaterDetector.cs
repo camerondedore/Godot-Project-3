0ec6fd7a88d4ef80a89b2db88c0258842b2879a5
camerondedore <camerondedore@gmail.com>
2025-07-22 20:41:12 -0700

    working on level 4

--- C#/RayCastWaterDetector.cs
+++ C#/RayCastWaterDetector.cs
@@ -5,8 +5,7 @@ using System.Collections;
 public partial class RayCastWaterDetector : RayCast3D
 {
     [Export]
-    PackedScene splashFx,
-        rippleFx;
+    PackedScene splashFx;
 
 
 
@@ -32,21 +31,6 @@ public partial class RayCastWaterDetector : RayCast3D
 
             // rotate fx
             newPrefab.Rotate(Vector3.Up, GD.Randf() * 3.14f);
-
-
-            // spawn ripple fx
-            var newPrefab2 = (Node3D) rippleFx.Instantiate();
-            
-            // assign parent and owner
-            GetTree().CurrentScene.AddChild(newPrefab2);
-            newPrefab2.Owner = GetTree().CurrentScene;
-
-            // place fx
-            newPrefab2.GlobalPosition = hitPoint + Vector3.Up * 0.03f;
-
-            // rotate fx
-            newPrefab2.Rotate(Vector3.Up, GD.Randf() * 3.14f);
-
 
             // delete water detector; only can hit water once
             QueueFree();
